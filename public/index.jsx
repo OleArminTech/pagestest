9e559cf4999e9210232bb852a1be64df6ed32f27
OleArminTech <37115964+OleArminTech@users.noreply.github.com>
2018-07-23 13:57:50 +0200

    Set up very basic react site.

---
+++ public/index.jsx
@@ -0,0 +1,12 @@
+// React, React-Router and Redux imports
+import React from 'react'
+import { render } from 'react-dom'
+
+// Non-package imports
+import App from '../src/app'
+
+
+render(
+  <App />,
+  document.getElementById('root')
+)
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: [
+    'babel-polyfill',
+    'webpack-hot-middleware/client',
+    './public/index.jsx'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: [/node_modules/],
+        query: {
+          presets: ['env', 'react', 'react-hmre', 'stage-2']
+        }
+      },
+      {
+        test: /\.(?:png|jpg)$/,
+        loader: 'url-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  }
+}
